dfcb55ee0858edb17e341584c8606495a4b77eeb
m2m-relations_ <moveout@yandex.ru>
2025-07-13 23:51:03 +0300

    1234

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const webpack = require('webpack');
+//const webpack = require('webpack');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 import webpack from 'webpack';
